df892be2c82e76ba8ab7a4e86759fdcc842e3af9
Olingo Maxwell <69717524+olingomaxwell1999@users.noreply.github.com>
2022-12-28 07:56:45 +0300

    More on the site

--- Components/Property/Property.js
+++ Components/Property/Property.js
@@ -14,8 +14,8 @@ import millify from "millify";
 
 import defaultImage from '../../assets/nyayo.jpg'
 
-const Property = ({property: {coverPhoto,price, rentFrequency, rooms, title, baths,area, agency, isVerified, externalId}}) => (
-    <Link href={`/property/${externalId}`} passHref>
+const Property = ({property: {coverPhoto,price, rentFrequency, rooms, title, baths,area, agency, isVerified, externalID}}) => (
+    <Link href={`/property/${externalID}`} passHref>
         <Flex flexWrap='wrap' w='420px' p='5' paddingTop='0' justifyContent='flex-start' cursor='pointer'>
             <Box>
                 <Image src={coverPhoto ? coverPhoto.url : defaultImage} alt='house' width={400} height={260} />
